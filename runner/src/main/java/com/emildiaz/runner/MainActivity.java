25d84898cee696149136af91260ae6f597d62ae1
Emil Diaz <emil.h.diaz@gmail.com>
2015-02-08 23:29:18 -0500

    Wiring buttons on main activity to new activities

--- runner/src/main/java/com/emildiaz/runner/MainActivity.java
+++ runner/src/main/java/com/emildiaz/runner/MainActivity.java
@@ -6,11 +6,44 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 
+import com.emildiaz.runner.model.Run;
+import com.google.android.gms.maps.model.LatLng;
+
+import java.util.TimeZone;
+
+import hirondelle.date4j.DateTime;
 import roboguice.activity.RoboActivity;
 import roboguice.inject.ContentView;
 
 @ContentView(R.layout.activity_main)
 public class MainActivity extends RoboActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+//        populateDummyData();
+    }
+
+    private void populateDummyData() {
+        Run run1 = new Run(DateTime.now(TimeZone.getDefault()).minusDays(2));
+        run1.save();
+        run1.addPoint(new LatLng(40.7532238, -73.9775961));
+        run1.addPoint(new LatLng(40.7365646, -73.989071));
+        run1.addPoint(new LatLng(40.737098, -73.9903523));
+        run1.addPoint(new LatLng(40.7359162, -73.9912157));
+
+        Run run2 = new Run(DateTime.now(TimeZone.getDefault()).minusDays(1));
+        run2.save();
+        run2.addPoint(new LatLng(40.7532238, -73.9775961));
+        run2.addPoint(new LatLng(40.7508586, -73.9786452));
+        run2.addPoint(new LatLng(40.7494838, -73.9753684));
+        run2.addPoint(new LatLng(40.7615555, -73.9665923));
+        run2.addPoint(new LatLng(40.7606092, -73.96432779999999));
+
+        run2.addPoint(new LatLng(40.7591407, -73.9598489));
+        run2.addPoint(new LatLng(40.7505916, -73.9401946));
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -24,8 +57,13 @@ public class MainActivity extends RoboActivity {
         return id == R.id.action_settings || super.onOptionsItemSelected(item);
     }
 
-    public void trackActivity(View view) {
-        Intent intent = new Intent(this, RunnerMapActivity.class);
+    public void newRun(View view) {
+        Intent intent = new Intent(this, RunActivity.class);
+        startActivity(intent);
+    }
+
+    public void viewRunHistory(View view) {
+        Intent intent = new Intent(this, RunHistoryActivity.class);
         startActivity(intent);
     }
 }
