a257ffc902676aff7f2a77c561d250e5e2b7b0cd
Emil Diaz <emil.h.diaz@gmail.com>
2015-02-09 20:36:08 -0500

    Updating test data so that durations are more reasonable

--- runner/src/main/java/com/emildiaz/runner/MainActivity.java
+++ runner/src/main/java/com/emildiaz/runner/MainActivity.java
@@ -26,14 +26,14 @@ public class MainActivity extends RoboActivity {
     }
 
     private void populateDummyData() {
-        Run run1 = new Run(DateTime.now(TimeZone.getDefault()).minusDays(2));
+        Run run1 = new Run(DateTime.now(TimeZone.getDefault()).minus(0, 0, 0, 0, 27, 0, 0, DateTime.DayOverflow.Spillover));
         run1.save();
         run1.addPoint(new LatLng(40.7532238, -73.9775961));
         run1.addPoint(new LatLng(40.7365646, -73.989071));
         run1.addPoint(new LatLng(40.737098, -73.9903523));
         run1.addPoint(new LatLng(40.7359162, -73.9912157));
 
-        Run run2 = new Run(DateTime.now(TimeZone.getDefault()).minusDays(1));
+        Run run2 = new Run(DateTime.now(TimeZone.getDefault()).minus(0, 0, 0, 0, 36, 0, 0, DateTime.DayOverflow.Spillover));
         run2.save();
         run2.addPoint(new LatLng(40.7532238, -73.9775961));
         run2.addPoint(new LatLng(40.7508586, -73.9786452));
